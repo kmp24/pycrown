3c80ee658a58c8a4285295a045aa85aebc74b91d
Perham <kperham@sanborn.com>
2025-09-02 21:44:03 -0400

    update requirements & add install_notes for upgrade to python 38, add toml and rudimentary scripts to preprocess lidar and run canopy model

---
+++ pycrown/process_tile.bat
@@ -0,0 +1,4 @@
+cd C:\Users\kperham\work\ny_canopy
+conda activate pycrown
+python C:\Users\kperham\work\development\pycrown\pycrown\process_lidar.py
+python C:\Users\kperham\work\development\pycrown\pycrown\process_canopy.py
