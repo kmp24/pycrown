0060504e687db5e7e658fca76ca932408f05affc
Perham <kperham@sanborn.com>
2025-09-02 22:00:08 -0400

    update

--- pycrown/constants.py
+++ pycrown/constants.py
@@ -1,2 +1,2 @@
-INFILE = r'A:\Services\Clients\c000.appgeo\Treez\data\e1463n2322_2019.las'
+INFILE = r'2019.las'
 EPSG = "EPSG:5070"
--- pycrown/process_tile.bat
+++ pycrown/process_tile.bat
@@ -1,4 +1,4 @@
-cd C:\Users\kperham\work\ny_canopy
+cd C:\Users\kperham\work\canopy
 conda activate pycrown
 python C:\Users\kperham\work\development\pycrown\pycrown\process_lidar.py
 python C:\Users\kperham\work\development\pycrown\pycrown\process_canopy.py
